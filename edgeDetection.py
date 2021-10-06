74b0cefcc62c2b79d1dd7a6ff2aaff57d421262e
ajcrosbie <aaron.j.crosbie@gmail.com>
2021-10-06 19:41:44 +0100

    folder detection added

--- edgeDetection.py
+++ edgeDetection.py
@@ -1,10 +1,26 @@
 import cv2
-import numpy as np
+import os
 
-imagePath = "e2.jpg"
+def oneImage(img, name):
+    os.chdir("..")
+    os.chdir("outputImages")
 
-img = cv2.imread(imagePath, 0)
-cv2.imwrite("edges2.jpg", cv2.Canny(img, 200, 300))
-cv2.imshow("edges2", cv2.imread("edges2.jpg"))
-cv2.waitKey()
-cv2.destroyAllWindows()
+
+    name = "lines" + name
+    cv2.imwrite(name, cv2.Canny(img, 200, 300))
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+def folder():
+    filePath = "images"
+    os.chdir(filePath)
+    files = os.listdir(os.getcwd())
+    for f in range(len(files)):
+        if files[f].endswith(".jpg") or files[f].endswith(".png"):
+            img = cv2.imread(files[f], 0)
+            oneImage(img, files[f])
+            os.chdir("..")
+            os.chdir(filePath)
+            
+if __name__ == "__main_":
+    folder()
