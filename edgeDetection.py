1e12d7f727c7c2469916d05a1c8688b5748d2389
ajcrosbie <aaron.j.crosbie@gmail.com>
2021-10-06 19:59:48 +0100

    small change

--- edgeDetection.py
+++ edgeDetection.py
@@ -4,9 +4,7 @@ import os
 def oneImage(img, name):
     os.chdir("..")
     os.chdir("outputImages")
-
-
-    name = "lines" + name
+    name = "lines_" + name
     cv2.imwrite(name, cv2.Canny(img, 200, 300))
     cv2.waitKey()
     cv2.destroyAllWindows()
@@ -15,6 +13,7 @@ def folder():
     filePath = "images"
     os.chdir(filePath)
     files = os.listdir(os.getcwd())
+
     for f in range(len(files)):
         if files[f].endswith(".jpg") or files[f].endswith(".png"):
             img = cv2.imread(files[f], 0)
@@ -22,5 +21,5 @@ def folder():
             os.chdir("..")
             os.chdir(filePath)
             
-if __name__ == "__main_":
+if __name__ == "__main__":
     folder()
